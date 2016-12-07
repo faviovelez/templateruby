b5460d30bc5d395a99ba873b3e13460df9986b17
FAVIO VELEZ <favio.velez@hotmail.com>
2016-12-06 21:51:33 -0600

    carriewave corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   has_many :subjects
   has_many :roles
+
+  mount_uploader :file, FileUploader
 
   def admin?
     !!(roles.find_by_name('Admin'))
@@ -18,7 +21,5 @@ class User < ActiveRecord::Base
   def editor?
     !!(roles.find_by_name('Editor'))
   end
-
-  
 
 end
